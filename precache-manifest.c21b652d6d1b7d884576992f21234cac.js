3d8ca2af039e219bb5f49e523617342091fc3059
Jimmy <jimjy98@gmail.com>
2021-11-19 17:59:47 -0500

    Updates

---
+++ precache-manifest.c21b652d6d1b7d884576992f21234cac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50603ce92af5bf6c40610d3147f3deac",
+    "url": "/investopedia/index.html"
+  },
+  {
+    "revision": "0d8e529f9052e1920e74",
+    "url": "/investopedia/static/css/main.b5dd3e23.chunk.css"
+  },
+  {
+    "revision": "8b8f741a7dfd30ef56bd",
+    "url": "/investopedia/static/js/2.b820fea1.chunk.js"
+  },
+  {
+    "revision": "6ed0ef5d376540dd82024d9d712fa3a5",
+    "url": "/investopedia/static/js/2.b820fea1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d8e529f9052e1920e74",
+    "url": "/investopedia/static/js/main.6e2b913f.chunk.js"
+  },
+  {
+    "revision": "31cdee2da2dc15a0ec98",
+    "url": "/investopedia/static/js/runtime-main.b43ef25f.js"
+  }
+]);
