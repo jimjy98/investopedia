b3279dc970b0e3cb068e7901b04d197b50c05403
Jimmy Khairallah <jimjy98@gmail.com>
2021-11-26 01:19:28 -0500

    news panel implementation

--- src/components/NewsPanel.js
+++ src/components/NewsPanel.js
@@ -1,10 +1,37 @@
-import { Typography } from '@mui/material';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Typography, TableContainer, Table, TableBody, TableRow, TableCell } from '@mui/material';
+import { getWatchlist } from '../api';
 
 export default function NewsPanel() {
+    const [watchlist, setWatchlist] = useState([]);
+
+    useEffect(() => {
+        const getWL = async () => {
+            setWatchlist(await getWatchlist());
+        };
+
+        getWL();
+    }, []);
+
     return (
         <div>
-            <Typography>bla3o baydate</Typography>
+            <Typography variant="h2" color="lightgrey">News</Typography>
+            <TableContainer >
+                <Table sx={{ width: '100%' }} aria-label="simple table">
+                    <TableBody>
+                        {watchlist.map((row) => (
+                            <TableRow
+                                key={row.ticker}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 }, backgroundColor: 'black' }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    <Typography color='lightgrey' >{row.ticker} "some news here (api costs money lol)"</Typography>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </div>
     )
 }
